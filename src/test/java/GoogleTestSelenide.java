78ad5d56730cafffb736c024df1e40ab9f8c29af
Igor_Kot <ilitviakov@gmail.com>
2018-08-02 04:15:58 +0300

    Done

---
+++ src/test/java/GoogleTestSelenide.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class GoogleTestSelenide {
+
+        @Test
+        public void login() {
+            open("https://www.google.com/");
+            $(By.name("q")).setValue("Hello world").pressEnter();
+            $$(By.className("g")).shouldHave(CollectionCondition.size(10));
+            $(By.className("g")).shouldHave(Condition.text("Hello world! — Вікіпедія"));
+        }
+
+
+
+}
